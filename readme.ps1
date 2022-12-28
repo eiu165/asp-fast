b01cb9b8aed5c3ca4dcbf99e313f403fb8428acf
Nick Macellaio <nmacellaio@hireology.com>
2022-12-28 17:33:34 -0600

    added ui

--- readme.ps1
+++ readme.ps1
@@ -4,4 +4,12 @@
 
 
 
+   88  dotnet new webapp -n app --dry-run 
+   89  dotnet new webapp -n app
+   138  cd Pages
+   139  dotnet new page -n ui --dry-run
+   140  dotnet new page -n ui
+
 dotnet run --urls=https://localhost:5001/  
+
+ 
